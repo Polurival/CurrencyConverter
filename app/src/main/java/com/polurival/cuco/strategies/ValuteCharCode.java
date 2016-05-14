6d029d0b7a441324db9cd90a1127f73ff38f92a0
Polurival <polurival@gmail.com>
2016-05-14 14:02:29 +0300

    resources updating

--- app/src/main/java/com/polurival/cuco/strategies/ValuteCharCode.java
+++ app/src/main/java/com/polurival/cuco/strategies/ValuteCharCode.java
@@ -8,10 +8,22 @@ import com.polurival.cuco.R;
  * on 10.04.2016.
  */
 public enum ValuteCharCode {
-    RUB {
+    AED {
         @Override
         public String getName() {
-            return AppContext.getContext().getString(R.string.RUB);
+            return AppContext.getContext().getString(R.string.AED);
+        }
+    },
+    ALL {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.ALL);
+        }
+    },
+    AMD {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.AMD);
         }
     },
     AUD {
@@ -26,28 +38,17 @@ public enum ValuteCharCode {
             return AppContext.getContext().getString(R.string.AZN);
         }
     },
-    GBP {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.GBP);
-        }
-    },
-    AMD {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.AMD);
-        }
-    },
-    BYR {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.BYR);
-        }
-    },
+
     BGN {
         @Override
         public String getName() {
             return AppContext.getContext().getString(R.string.BGN);
+        }
+    },
+    BHD {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.BHD);
         }
     },
     BRL {
@@ -56,136 +57,17 @@ public enum ValuteCharCode {
             return AppContext.getContext().getString(R.string.BRL);
         }
     },
-    HUF {
+    BYR {
         @Override
         public String getName() {
-            return AppContext.getContext().getString(R.string.HUF);
+            return AppContext.getContext().getString(R.string.BYR);
         }
     },
-    DKK {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.DKK);
-        }
-    },
-    USD {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.USD);
-        }
-    },
-    EUR {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.EUR);
-        }
-    },
-    INR {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.INR);
-        }
-    },
-    KZT {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.KZT);
-        }
-    },
+
     CAD {
         @Override
         public String getName() {
             return AppContext.getContext().getString(R.string.CAD);
-        }
-    },
-    KGS {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.KGS);
-        }
-    },
-    CNY {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.CNY);
-        }
-    },
-    MDL {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.MDL);
-        }
-    },
-    NOK {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.NOK);
-        }
-    },
-    PLN {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.PLN);
-        }
-    },
-    RON {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.RON);
-        }
-    },
-    XDR {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.XDR);
-        }
-    },
-    SGD {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.SGD);
-        }
-    },
-    TJS {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.TJS);
-        }
-    },
-    TRY {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.TRY);
-        }
-    },
-    TMT {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.TMT);
-        }
-    },
-    UZS {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.UZS);
-        }
-    },
-    UAH {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.UAH);
-        }
-    },
-    CZK {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.CZK);
-        }
-    },
-    SEK {
-        @Override
-        public String getName() {
-            return AppContext.getContext().getString(R.string.SEK);
         }
     },
     CHF {
@@ -194,10 +76,113 @@ public enum ValuteCharCode {
             return AppContext.getContext().getString(R.string.CHF);
         }
     },
-    ZAR {
+    CNY {
         @Override
         public String getName() {
-            return AppContext.getContext().getString(R.string.ZAR);
+            return AppContext.getContext().getString(R.string.CNY);
+        }
+    },
+    CZK {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.CZK);
+        }
+    },
+
+    DKK {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.DKK);
+        }
+    },
+
+    EEK {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.EEK);
+        }
+    },
+    EGP {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.EGP);
+        }
+    },
+    EUR {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.EUR);
+        }
+    },
+
+    GBP {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.GBP);
+        }
+    },
+
+    HRK {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.HRK);
+        }
+    },
+    HUF {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.HUF);
+        }
+    },
+
+    ILS {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.ILS);
+        }
+    },
+    INR {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.INR);
+        }
+    },
+    IQD {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.IQD);
+        }
+    },
+    IRR {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.IRR);
+        }
+    },
+    ISK {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.ISK);
+        }
+    },
+
+    JOD {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.JOD);
+        }
+    },
+    JPY {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.JPY);
+        }
+    },
+
+    KGS {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.KGS);
         }
     },
     KRW {
@@ -206,10 +191,191 @@ public enum ValuteCharCode {
             return AppContext.getContext().getString(R.string.KRW);
         }
     },
-    JPY {
+    KWD {
         @Override
         public String getName() {
-            return AppContext.getContext().getString(R.string.JPY);
+            return AppContext.getContext().getString(R.string.KWD);
+        }
+    },
+    KZT {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.KZT);
+        }
+    },
+
+    LBP {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.LBP);
+        }
+    },
+    LTL {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.LTL);
+        }
+    },
+    LVL {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.LVL);
+        }
+    },
+
+    MDL {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.MDL);
+        }
+    },
+    MKD {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.MKD);
+        }
+    },
+
+    NOK {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.NOK);
+        }
+    },
+    NZD {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.NZD);
+        }
+    },
+
+    OMR {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.OMR);
+        }
+    },
+
+    PLN {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.PLN);
+        }
+    },
+
+    QAR {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.QAR);
+        }
+    },
+
+    RON {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.RON);
+        }
+    },
+    RUB {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.RUB);
+        }
+    },
+
+    SAR {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.SAR);
+        }
+    },
+    SEK {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.SEK);
+        }
+    },
+    SGD {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.SGD);
+        }
+    },
+    SKK {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.SKK);
+        }
+    },
+    SYP {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.SYP);
+        }
+    },
+
+    TJS {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.TJS);
+        }
+    },
+    TMT {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.TMT);
+        }
+    },
+    TND {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.TND);
+        }
+    },
+    TRY {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.TRY);
+        }
+    },
+
+    UAH {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.UAH);
+        }
+    },
+    USD {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.USD);
+        }
+    },
+    UZS {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.UZS);
+        }
+    },
+
+    XDR {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.XDR);
+        }
+    },
+
+    YER {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.YER);
+        }
+    },
+
+    ZAR {
+        @Override
+        public String getName() {
+            return AppContext.getContext().getString(R.string.ZAR);
         }
     };
 
